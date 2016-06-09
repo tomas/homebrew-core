500c9033502b9baf2c358b4d3b7c9c9a3f91000c
Tomás Pollak <tomaspollak@gmail.com>
2016-06-09 00:36:50 -0700

    Fixes as suggested by the homebrew team

--- Formula/git-webui.rb
+++ Formula/git-webui.rb
@@ -4,6 +4,7 @@ class GitWebui < Formula
   url "https://github.com/alberthier/git-webui/archive/v1.2.0.tar.gz"
   sha256 "21faa8a018d7325bd3acb7e7da138a2a61b504698f96fd067fa0ee765f3f15dd"
   head "https://github.com/alberthier/git-webui.git"
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
@@ -12,11 +13,12 @@ class GitWebui < Formula
 
     # make sure the original script resolves the path to the assets correctly
     # a patch was submitted to change this in the upstream repo:
-    # https://github.com/tomas/git-webui/commit/fd6a9d5c064d5e58742a0078e7842bbf70f2de33
+    # https://github.com/alberthier/git-webui/pull/24
     inreplace libexec/"git-core/git-webui", ".abspath(sys.argv[0])", ".realpath(sys.argv[0])"
   end
 
   test do
-    assert_equal "No git repository found", `#{bin}/git-webui --repo-root /tmp 2>&1`.strip
+    output = `#{bin}/git-webui --repo-root #{testpath} 2>&1`
+    assert_equal "No git repository found", output.strip
   end
 end
